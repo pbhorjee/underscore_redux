9a89f41fb51c808ed5e460483a8e412ba896d488
pbhorjee <paul@bhorjee.com>
2015-10-06 13:11:42 -0500

    Add reduce method

--- src/underbar.js
+++ src/underbar.js
@@ -179,6 +179,15 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      accumulator = collection.shift();
+    }
+
+    _.each(collection, function(item) {
+      accumulator = iterator(accumulator, item);
+    });
+
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
