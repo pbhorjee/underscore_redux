5abb61f54f1d5f3f32c13197672c04b5f91bd7b3
pbhorjee <paul@bhorjee.com>
2015-10-06 13:35:34 -0500

    Add defaults method

--- src/underbar.js
+++ src/underbar.js
@@ -267,6 +267,17 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var args = Array.prototype.slice.call(arguments).slice(1);
+
+    _.each(args, function(arg) {
+      _.each(arg, function(val, key) {
+        if (obj[key] === undefined) {
+          obj[key] = val;
+        }
+      })
+    });
+
+    return obj;
   };
 
 
